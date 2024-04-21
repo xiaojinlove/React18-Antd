197eb2b3ad2deff58bff12e55c1228b8604d7462
gaoyan <magaoyanxj@gmail.com>
2024-04-21 10:11:07 +0800

    项目创建-配置antd和路由

--- react18-antd/src/main.jsx
+++ react18-antd/src/main.jsx
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import 'antd/dist/reset.css'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import { HashRouter as Router} from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+  <Router>
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
+  </Router>
 )
